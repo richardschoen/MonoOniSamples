47bce3e02d3e8de79d365a220cc25eaeb607fa28
Richard Schoen <richard@rjssoft.com>
2019-01-13 19:43:09 -0600

    Add Time zone list sample.

---
+++ MonoListTimeZones/MonoListTimeZones/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Collections.ObjectModel;
+
+namespace MonoListTimeZones
+{
+    /// <summary>
+    /// This is a sample MonoListTimeZones
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try {
+
+            // Example: Set TZ environment variable for desired timezone
+            // Note: You MUST set up TZ before calling any date/time functions. 
+            Environment.SetEnvironmentVariable("TZ", "America/Chicago");
+            Console.WriteLine("Sample timezone in TZ environment variable: America/Chicago");
+            Console.WriteLine("Current time once TZ env variable set: " + DateTime.Now);
+
+            // Display all timezone values. 
+            ReadOnlyCollection<TimeZoneInfo> zones = TimeZoneInfo.GetSystemTimeZones();
+            Console.WriteLine("The local system has the following {0} time zones", zones.Count);
+            Console.WriteLine("Set the TZ environment variable in your program \r\nto the desired time zone value before using date/time functions.");
+            Console.WriteLine("Time Zone List");
+            foreach (TimeZoneInfo zone in zones)
+              Console.WriteLine(zone.Id);
+
+            Environment.ExitCode = 0;             
+
+            }
+         catch (Exception ex)
+            {
+                // Write error message to console/STDOUT log and set exit code 
+                // to 99 to indicate an error to the Operating System. 
+                Console.WriteLine("Error: " + ex.Message);
+                Environment.ExitCode = 99;
+            }
+            finally
+            {
+                // Exit the program
+                Console.WriteLine("ExitCode: " + Environment.ExitCode);
+                Environment.Exit(Environment.ExitCode);
+            }
+
+        }
+    }
+}
---
+++ MonoListTimeZones/MonoListTimeZones/README.md
@@ -0,0 +1,40 @@
+﻿# MonoListTimeZones
+
+**Overview**<br>
+This example is meant to list all time zone values that you can possibly select to STDOUT.<br>
+Mono does not correctly observe the IBMi or PASE timezone settings so we need to set a local
+environment variable named: TZ to set the desired local timezone
+
+**Running the program from a PASE or QSHELL command line screen**<br>
+`cd /MonoOniSamples/MonoHelloWorld (or the directory where the EXE was created.)`
+`mono MonoHelloWorld.exe`
+
+**Running the program using the MONO CL command**<br>
+```
+MONO WORKDIR('/MonoOniSamples/MonoListTimeZones')   
+     EXEFILE(MonoListTimeZones.exe)                 
+     DSPSTDOUT(*YES)                             
+     DLTSTDOUT(*YES)
+```
+**Compiling and building this program solution**<br>
+
+1.) The easiest way to build this .Net code for IBMi is to compile locally on a PC in Visual Studio
+and then copy the EXE and any associated DLL files to the associated app directory in the IFS. 
+I typically put each app and DLL files in its own directory. This way each app can have different
+version of a class library or other DLL being used without worrying about DLL versioning issues.
+
+2.) An alternative option is to build the solution from the PASE command line:
+
+Start pase command line terminal:<br>
+`CALL QP2TERM`
+
+Change to the selected app folder where EXE is located:<br>
+`cd /MonoOniSamples/MonoListTimeZones`
+
+
+Call the following command line to build the Visual Studio Solution natively on i:<br>
+`xbuild /p:CscToolExe=mcs`
+
+If all ran successfully you should have a compiled EXE or DLL in your build dir of:<br>
+`/MonoOniSamples/MonoListTimeZones/MonoListTimeZones/bin/Debug`
+
